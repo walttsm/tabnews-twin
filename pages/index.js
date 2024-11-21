6b35df7d7f4fc9c51a2265a47fc277943c42f5ce
Walter Marinho <waltmarinho@gmail.com>
2024-11-21 17:35:07 -0300

    feat: adicionando diretório pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Aqui era pra ter uma mensagem pra alguém ser impactado, espero que o
+      impactado seja eu mesmo.
+    </h1>
+  );
+}
+
+export default Home;
