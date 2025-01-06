44b50a095b0215ba64f6b20d8f05d93781a2e8e5
Walter Marinho <waltmarinho@gmail.com>
2025-01-06 16:32:42 -0300

    fix: ajuste frase para impacto

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ function Home() {
   return (
     <h1>
       Aqui era pra ter uma mensagem pra alguém ser impactado, espero que o
-      impactado seja eu mesmo.
+      impactado seja eu mesmo e espero também que você, que está lendo isso seja
+      impactado também!!!
     </h1>
   );
 }
